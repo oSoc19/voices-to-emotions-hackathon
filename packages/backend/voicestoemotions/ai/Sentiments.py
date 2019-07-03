385597a2eebdbc592d11018a44256729e0924ba5
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-03 18:07:40 +0200

    weird shit

--- packages/backend/voicestoemotions/ai/Sentiments.py
+++ packages/backend/voicestoemotions/ai/Sentiments.py
@@ -4,30 +4,20 @@ import os
 import numpy
 import pandas
 
-from keras.layers import Dense, Conv2D, Activation, MaxPooling2D, Dropout, Flatten
+from keras.layers import Dense, Conv2D, Activation, MaxPooling2D, Dropout, Flatten, BatchNormalization
 from keras.models import Sequential
 from keras.optimizers import Adam
-from keras.preprocessing import sequence
-from keras.preprocessing.text import Tokenizer
-from keras.utils import to_categorical
-import tensorflow
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import EarlyStopping
-from sklearn.model_selection import train_test_split
+from keras.constraints import maxnorm
 
-from nltk.tokenize import word_tokenize
-from nltk import FreqDist
-from nltk.stem import WordNetLemmatizer
-
-import re
 
 def main():
     gc.collect()
 
-    data_dir = os.path.abspath('../../../../voices-to-emotions/voices-to-emotions-dataset')
+    data_dir = os.path.abspath('../../../../voices-to-emotions')
     index_df = pandas.read_csv(os.path.join(data_dir, 'index_goodbad.csv'))
-
-    # emotions = ['angry', 'fearful', 'disgust', 'sad', 'happy', 'neutral', 'calm', 'surprised']
+    class_num = 2
 
     datagen = ImageDataGenerator(width_shift_range=0.6)
 
@@ -52,34 +42,45 @@ def main():
         seed=42,
         target_size=(223, 221))
 
-    # test_generator = datagen.flow_from_dataframe(
-    #     dataframe=test,
-    #     directory=data_dir,
-    #     x_col="file_path",
-    #     y_col="emotion",
-    #     seed=42,
-    #     target_size=(223, 221))
-
-
     model = Sequential([
-    Conv2D(32, (3, 3), padding='same', input_shape=(223, 221, 3)),
-    MaxPooling2D(pool_size=(2, 2)),
-    Dropout(0.25),
-    Conv2D(64, (3, 3), padding='same'),
-    MaxPooling2D(pool_size=(2, 2)),
-    Dropout(0.5),
-    Flatten(),
-    Dense(512),
-    Activation('relu'),
-    Dropout(0.5),
-    Dense(2, activation='softmax')])
+        Conv2D(32, (3, 3), padding='same', input_shape=(223, 221, 3)),
+        Activation('relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.3),
+        BatchNormalization(),
+
+        Conv2D(64, (3, 3), padding='same', input_shape=(223, 221, 3)),
+        Activation('relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.3),
+        BatchNormalization(),
+
+        Flatten(),
+        Dropout(0.2),
+
+        # Dense(256, kernel_constraint=maxnorm(3)),
+        # Activation('relu'),
+        # Dropout(0.2),
+        # BatchNormalization(),
+
+        # Dense(128, kernel_constraint=maxnorm(3)),
+        # Activation('relu'),
+        # Dropout(0.2),
+        # BatchNormalization(),
+
+        Dense(class_num),
+        Activation('selu'),
+    ])
     model.compile(optimizer=Adam(lr=0.001), loss="categorical_crossentropy", metrics=["accuracy"])
-    model.fit_generator(generator=train_generator, validation_data=validate_generator, steps_per_epoch=15, validation_steps=15, epochs=50, callbacks=[EarlyStopping(monitor='loss', mode='min', verbose=1, patience=5)])
+    model.fit_generator(generator=train_generator, validation_data=validate_generator, steps_per_epoch=15,
+                        validation_steps=15, epochs=50,
+                        callbacks=[EarlyStopping(monitor='loss', mode='min', verbose=1, patience=4)])
 
     model_json = model.to_json()
     with open("model.json", "w") as json_file:
-      json_file.write(model_json)
+        json_file.write(model_json)
     model.save_weights("model.h5")
+
 
 if __name__ == "__main__":
     main()
