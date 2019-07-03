7892f8c9d18f099e237b079e6d3eefbbf3d65e45
Tib <thibaut-c@hotmail.com>
2019-07-03 15:02:48 +0200

    feat(django): speech to image

--- packages/backend/voicestoemotions/ai/views.py
+++ packages/backend/voicestoemotions/ai/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
+from django.core.files.storage import FileSystemStorage
+from .speechtoimage import graph_spectrogram
 
 @api_view(['POST'])
 def upload_file(request):
-    #if request.method == 'POST':
-        #handle_uploaded_file(request.FILES['file'])
-    return JsonResponse({'text':"abc",'emotions':{'angry': 0.2, 'happy':0.3}})
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+
+        graph_spectrogram(uploaded_file_url)
+
+        return JsonResponse(uploaded_file_url,safe=False)
+    return JsonResponse("oops something wrong")
