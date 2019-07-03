fc2c8046013d824d2081a755d8124c4309dc63be
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-03 14:31:40 +0200

    change UI to new API

--- packages/frontend/src/components/result-page.tsx
+++ packages/frontend/src/components/result-page.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { H1, H2 } from '@blazingly-design/heading';
 import Paragraph from '@blazingly-design/paragraph';
 
 import Loader from '../components/loader';
 
+export type EmotionType = 'neutral' | 'calm' | 'happy' | 'surprised' | 'sad' | 'angry' | 'fearful' | 'disgust';
+
 export type ResultType = {
   text: string;
-  emotion: number;
+  emotion: EmotionType;
 };
 
 export type DropzonePageProps = {
   file: FileList;
+};
+
+export const positiveEmotions = ['neutral', 'calm', 'happy'];
+
+export const emojiMap = {
+  neutral: require('../images/neutral.svg'),
+  calm: require('../images/calm.svg'),
+  happy: require('../images/happy.svg'),
+  surprised: require('../images/surprised.svg'),
+  sad: require('../images/sad.svg'),
+  angry: require('../images/angry.svg'),
+  fearful: require('../images/fearful.svg'),
+  disgust: require('../images/disgust.svg')
 };
 
 const Wrapper = styled.div(() => {
@@ -48,8 +62,8 @@ export default function DropzonePage(props: DropzonePageProps) {
   React.useEffect(() => {
     setTimeout(() => {
       setRes({
-        text: 'This should be a transcript of the audio file...',
-        emotion: 0.3
+        text: 'I am a very sad person in a very sad world.',
+        emotion: 'sad'
       });
     }, 2500);
   }, [true]);
@@ -57,8 +71,8 @@ export default function DropzonePage(props: DropzonePageProps) {
   return (
     <>
       <Wrapper>
-        <Loader finished={!!res} positive={res ? res.emotion < 0.5 : undefined}>
-          {res ? Math.round(res.emotion * 100) : ''}
+        <Loader finished={!!res} positive={res ? positiveEmotions.includes(res.emotion) : undefined}>
+          {res ? <img src={emojiMap[res.emotion]} alt={res.emotion} title={res.emotion} /> : ''}
         </Loader>
       </Wrapper>
       <TextBalloon hasContent={!!res}>
