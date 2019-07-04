fe5670b44d092c94f151ca91d70de0d7e555c984
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-04 07:44:42 +0200

    cleanup api code

--- packages/backend/voicestoemotions/ai/views.py
+++ packages/backend/voicestoemotions/ai/views.py
@@ -1,27 +1,33 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
 from django.core.files.storage import FileSystemStorage
 from .speechtoimage import create_spectrogram
 import os
+import datetime
+
+upload_dir = './uploaded'
+
 
 @api_view(['POST'])
 def upload_file(request):
     if request.method == 'POST' and request.FILES['myfile']:
+        file_name = datetime.datetime.now().isoformat() + '.wav'
+        save_path = os.path.join(upload_dir, file_name)
+
         myfile = request.FILES['myfile']
+
         fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
+        filename = fs.save(save_path, myfile)
         uploaded_file_url = fs.url(filename)
 
-        create_spectrogram(uploaded_file_url)
+        spectrogram_filename = create_spectrogram(uploaded_file_url)
 
-        os.remove(uploaded_file_url)
+        # TODO: Predict based on this filename
 
-        newName = uploaded_file_url.split(".")[0] + ".png"
+        result = {
+            'text': "text",
+            'emotions': "sad"
+        }
 
-        result = {'text':"text","emotions":"sad"} # send newName to AI
-
-        os.remove(newName)
-
-        return JsonResponse(result,safe=False)
+        return JsonResponse(result, safe=False)
     return JsonResponse("oops something wrong")
