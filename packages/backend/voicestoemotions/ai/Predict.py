d53a1afcad21c360de528c1680828baa0ab6614b
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-04 07:54:52 +0200

    format predict code so pycharm stops complaining

--- packages/backend/voicestoemotions/ai/Predict.py
+++ packages/backend/voicestoemotions/ai/Predict.py
@@ -6,27 +6,30 @@ import pandas as pd
 
 
 def get_prediction(filepath):
-  dir = os.path.dirname(filepath)
-  file_name = os.path.basename(filepath)
+    dir = os.path.dirname(filepath)
+    file_name = os.path.basename(filepath)
 
-  df = pd.DataFrame([{ 'file_path': file_name }])
+    df = pd.DataFrame([{'file_path': file_name}])
 
-  loaded_model = load_model(os.path.abspath('./model.h5'))
+    loaded_model = load_model(os.path.abspath('./model.h5'))
 
-  test_datagen=ImageDataGenerator(rescale=1./255.)
-  test_gen = test_datagen.flow_from_dataframe(
-    dataframe=df,
-    directory=dir,
-    x_col="file_path",
-    y_col=None,
-    subset="training",
-    batch_size=1,
-    class_mode=None,
-    target_size=(64, 64)
-  )
+    test_datagen = ImageDataGenerator(rescale=1. / 255.)
+    test_gen = test_datagen.flow_from_dataframe(
+        dataframe=df,
+        directory=dir,
+        x_col="file_path",
+        y_col=None,
+        subset="training",
+        batch_size=1,
+        class_mode=None,
+        target_size=(64, 64)
+    )
 
-  pred = loaded_model.predict_generator(test_gen, steps=1, verbose=1)
+    pred = loaded_model.predict_generator(test_gen, steps=1, verbose=1)
 
-  emotions = ['angry', 'fearful', 'disgust', 'sad', 'surprised', 'happy', 'calm', 'neutral']
+    print('Predictions')
+    print(pred)
 
-  return emotions[np.argmax(pred, axis=1)[0]]
+    emotions = ['angry', 'fearful', 'disgust', 'sad', 'surprised', 'happy', 'calm', 'neutral']
+
+    return emotions[np.argmax(pred, axis=1)[0]]
