3a2b449cf5847f94cc8632845938363eae943422
Jasper De Moor <jasperdemoor@gmail.com>
2019-07-04 09:01:45 +0200

    cleanup data input flow

--- packages/backend/voicestoemotions/ai/Sentiments.py
+++ packages/backend/voicestoemotions/ai/Sentiments.py
@@ -16,13 +16,11 @@ def main():
     data_dir = os.path.abspath('./dataset')
     index_df = pandas.read_csv(os.path.join(data_dir, 'index.csv'))
 
-    datagen = ImageDataGenerator(rescale=1. / 255., validation_split=0.25)
-
-    train = index_df.iloc[:round(len(index_df) * .6)]
-    validate = index_df.iloc[round(len(index_df) * .6):]
+    train = index_df.iloc[:round(len(index_df) * .9)]
     test = index_df.iloc[round(len(index_df) * .9):]
 
-    train_generator = datagen.flow_from_dataframe(
+    train_datagen = ImageDataGenerator(rescale=1. / 255., validation_split=0.25)
+    train_generator = train_datagen.flow_from_dataframe(
         dataframe=train,
         directory=data_dir,
         x_col="file_path",
@@ -30,23 +28,14 @@ def main():
         batch_size=32,
         seed=42,
         shuffle=True,
+        subset="training",
         class_mode="categorical",
         target_size=(64, 64))
 
-    validate_generator = datagen.flow_from_dataframe(
-        dataframe=validate,
+    validation_generator = train_datagen.flow_from_dataframe(
+        dataframe=train,
         directory=data_dir,
-        x_col="file_path",
-        y_col="emotion",
-        batch_size=32,
-        seed=42,
-        shuffle=True,
-        class_mode="categorical",
-        target_size=(64, 64))
-
-    test_generator = datagen.flow_from_dataframe(
-        dataframe=test,
-        directory=data_dir,
+        subset="validation",
         x_col="file_path",
         y_col="emotion",
         batch_size=32,
@@ -81,16 +70,16 @@ def main():
     gc.collect()
 
     step_size_generator = train_generator.n // train_generator.batch_size
-    step_size_validate = validate_generator.n // validate_generator.batch_size
+    step_size_validate = validation_generator.n // validation_generator.batch_size
 
     model.fit_generator(
         generator=train_generator,
-        validation_data=validate_generator,
+        validation_data=validation_generator,
         steps_per_epoch=step_size_generator,
         validation_steps=step_size_validate,
         epochs=100,
         use_multiprocessing=True,
-        callbacks=[EarlyStopping(monitor='loss', mode='min', verbose=1, patience=5)]
+        # callbacks=[EarlyStopping(monitor='loss', mode='min', verbose=1, patience=5)]
     )
 
     gc.collect()
@@ -102,6 +91,18 @@ def main():
     print('Model Saved!')
 
     print('Evaluating model...')
+
+    test_datagen = ImageDataGenerator(rescale=1. / 255.)
+    test_generator = test_datagen.flow_from_dataframe(
+        dataframe=test,
+        directory=data_dir,
+        x_col="file_path",
+        y_col="emotion",
+        batch_size=32,
+        seed=42,
+        shuffle=True,
+        class_mode="categorical",
+        target_size=(64, 64))
 
     test_steps = test_generator.n / test_generator.batch_size
     scores = model.evaluate_generator(generator=test_generator, steps=test_steps, verbose=1, use_multiprocessing=True)
